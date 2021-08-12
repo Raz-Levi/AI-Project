1bc725118c6b383b5f2ee57be95581aa73476045
Raz Levi <razle102030@gmail.com>
2021-08-12 19:10:04 +0300

    add abstract class for learning algorithm
    https://app.asana.com/0/1200730227122327/1200737749158691/f

--- Tests/unit_test.py
+++ Tests/unit_test.py
@@ -80,15 +80,43 @@ class TestUtils(unittest.TestCase):
 
 class TestLearningAlgorithm(unittest.TestCase):
     def test_initialization(self):
+        consts = self._get_consts()
+        simple_algorithm = self._get_instance()
+        self.assertTrue(simple_algorithm.predict(sample=consts["test_sample"], given_feature=consts["given_feature"],
+                                                 maximal_cost=consts["test_sample"]))
+
+    def test_fit(self):
+        consts = self._get_consts()
+        simple_algorithm = self._get_instance()
+        self.assertTrue(simple_algorithm._get_total_features_num() is None)
+        simple_algorithm.fit(consts["train_samples"], consts["features_costs"])
+        self.assertTrue(simple_algorithm._get_total_features_num() == consts["total_features_num"])
+
+    # private functions
+    @staticmethod
+    def _get_consts():
+        return {
+            "test_sample": np.ndarray([1]),
+            "train_samples": TrainSamples(np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [0, 1, 2]]).T, np.ndarray([1, 1, 1, 1])),
+            "given_feature": [1],
+            "total_features_num": 3,
+            "features_costs": [1],
+            "maximal_cost": 1
+        }
+
+    @staticmethod
+    def _get_instance():
         class SimpleAlgorithm(LearningAlgorithm):
-            def fit(self, train_samples: TrainSamples, features_costs: list[float]):
+            def _fit(self, train_samples: TrainSamples, features_costs: list[float]):
                 pass
 
             def predict(self, sample: TestSamples, given_feature: list[int], maximal_cost: float) -> int:
                 return True
 
-        simple_algorithm = SimpleAlgorithm()
-        self.assertTrue(simple_algorithm.predict(sample=np.ndarray([1]), given_feature=[1], maximal_cost=1))
+            def _get_total_features_num(self):
+                return self._total_features_num
+
+        return SimpleAlgorithm()
 
 
 if __name__ == '__main__':
--- abstract_algorithm.py
+++ abstract_algorithm.py
@@ -10,10 +10,19 @@ from utils import *
 
 
 class LearningAlgorithm(abc.ABC):
-    @abc.abstractmethod
+    def __init__(self):
+        self._total_features_num = None
+
+    ### Public Methods ###
     def fit(self, train_samples: TrainSamples, features_costs: list[float]):
-        ...
+        self._total_features_num = train_samples.samples.shape[0]
+        self._fit(train_samples, features_costs)
 
     @abc.abstractmethod
     def predict(self, sample: TestSamples, given_features: list[int], maximal_cost: float) -> int:
+        ...
+
+    ### Private Methods ###
+    @abc.abstractmethod
+    def _fit(self, train_samples: TrainSamples, features_costs: list[float]):
         ...
