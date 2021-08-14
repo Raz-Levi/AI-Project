14d994e573d2e1b56ef2620711f1ee720a718577
Raz Levi <razle102030@gmail.com>
2021-08-14 19:44:37 +0300

    add description for naive algorithms

--- naive_algorithm.py
+++ naive_algorithm.py
@@ -12,32 +12,78 @@ from abstract_algorithm import LearningAlgorithm
 
 
 class EmptyAlgorithm(LearningAlgorithm):
+    """
+    A naive algorithm that ignores the option to buy features and runs sklearn's classifier for samples filtering the\
+    given features only.
+    """
     def __init__(self, learning_algorithm: sklearn.base.ClassifierMixin):
+        """
+        Init function.
+        :param learning_algorithm: sklearn's classifier. the function saves it and uses it later.
+        """
         super().__init__()
         self._train_samples = None
         self._learning_algorithm = learning_algorithm
 
     def fit(self, train_samples: TrainSamples, features_costs: list[float]):
+        """
+        Trains the classifier. the function saves the train samples for the prediction.
+        :param train_samples: training dataset contains training data of shape (n_samples, n_features), i.e samples are
+        in the rows, and target values of shape (n_samples,). the function saves it.
+        :param features_costs: list in length number of features that contains the costs of each feature according to
+        indices. in first index you will find the cost of the first feature, etc. the function ignores it.
+        """
         self._train_samples = train_samples
 
     def predict(self, samples: TestSamples, given_features: list[int], maximal_cost: float) -> Classes:
+        """
+        Predicts the class labels for the provided data. the function runs the fit and predict function of the given
+        sklearn's classifier on the given test samples filtering the given features only.
+        :param samples: test samples of shape (n_samples, n_features), i.e samples are in the rows.
+        :param given_features: list of the indices of the chosen features.
+        :param maximal_cost: the maximum available cost for buying features.
+        :return: Classes of shape (n_samples,) contains the class labels for each data sample.
+        """
         self._learning_algorithm.fit(self._train_samples.samples[:, given_features], self._train_samples.classes)
         return self._learning_algorithm.predict(samples[:, given_features])
 
 
 class SequenceAlgorithm(LearningAlgorithm):
+    """
+    An abstract naive algorithm that chooses simply features to add to the given features according to the given budget.
+    """
     # Public Methods
     def __init__(self, learning_algorithm: sklearn.base.ClassifierMixin):
+        """
+        Init function.
+        :param learning_algorithm: sklearn's classifier. the function saves it and uses it later.
+        """
         super().__init__()
         self._train_samples = None
         self._features_costs = []
         self._learning_algorithm = learning_algorithm
 
     def fit(self, train_samples: TrainSamples, features_costs: list[float]):
+        """
+        Trains the classifier. the function saves the train samples and the features costs for the prediction.
+        :param train_samples: training dataset contains training data of shape (n_samples, n_features), i.e samples are
+        in the rows, and target values of shape (n_samples,). the function saves it.
+        :param features_costs: list in length number of features that contains the costs of each feature according to
+        indices. in first index you will find the cost of the first feature, etc. the function saves it.
+        """
         self._train_samples = train_samples
         self._features_costs = features_costs
 
     def predict(self, samples: TestSamples, given_features: list[int], maximal_cost: float) -> Classes:
+        """
+        Predicts the class labels for the provided data. the function chooses simply features to add to the given
+        features according to the given budget and runs the fit and predict function of the given sklearn's classifier
+        on the given test samples filtering the given features.
+        :param samples: test samples of shape (n_samples, n_features), i.e samples are in the rows.
+        :param given_features: list of the indices of the chosen features.
+        :param maximal_cost: the maximum available cost for buying features.
+        :return: Classes of shape (n_samples,) contains the class labels for each data sample.
+        """
         given_features = self._buy_features(given_features, maximal_cost)
         self._learning_algorithm.fit(self._train_samples.samples[:, given_features], self._train_samples.classes)
         return self._learning_algorithm.predict(samples[:, given_features])
@@ -45,15 +91,31 @@ class SequenceAlgorithm(LearningAlgorithm):
     # Private Methods
     @abc.abstractmethod
     def _buy_features(self, given_features: list[int], maximal_cost: float) -> list[int]:
+        """
+        An abstract method for choosing the supplementary features.
+        :param given_features: list of the indices of the chosen features.
+        :param maximal_cost: the maximum available cost for buying features.
+        :return: the updated given features including all the chosen features.
+        """
         ...
 
 
 class RandomAlgorithm(SequenceAlgorithm):
+    """
+    A naive algorithm that chooses randomly features to add to the given features according to the given budget.
+    """
     def __init__(self, learning_algorithm: sklearn.base.ClassifierMixin):
         super().__init__(learning_algorithm)
 
     # Private Methods
     def _buy_features(self, given_features: list[int], maximal_cost: float) -> list[int]:
+        """
+        A method for choosing the supplementary features. the methods chooses features randomly and stops if all the
+        features were chosen or if the cost of the supplementary features is above the maximal cost.
+        :param given_features: list of the indices of the chosen features.
+        :param maximal_cost: the maximum available cost for buying features.
+        :return: the updated given features including all the chosen features.
+        """
         available_features = set(range(self._train_samples.samples.shape[1])) - set(given_features)
         while len(available_features) and maximal_cost:
             chosen_feature = random.choice(list(available_features))
@@ -65,11 +127,23 @@ class RandomAlgorithm(SequenceAlgorithm):
 
 
 class OptimalAlgorithm(SequenceAlgorithm):
+    """
+    A naive algorithm that chooses the cheapest features to add to the given features according to the given budget.
+    """
     def __init__(self, learning_algorithm: sklearn.base.ClassifierMixin):
         super().__init__(learning_algorithm)
 
     # Private Methods
     def _buy_features(self, given_features: list[int], maximal_cost: float) -> list[int]:
+        """
+        A method for choosing the supplementary features. the methods chooses features according to their costs. the
+        features are sorted according to their costs, for the cheapest to the most expensive. the features are chosen
+        from the cheapest until all the features are chosen or the costs of the chosen features is above the maximal cost.
+        features were chosen or if the cost of the supplementary features is above the maximal cost.
+        :param given_features: list of the indices of the chosen features.
+        :param maximal_cost: the maximum available cost for buying features.
+        :return: the updated given features including all the chosen features.
+        """
         available_features = list(set(range(self._train_samples.samples.shape[1])) - set(given_features))
         available_features.sort(reverse=True, key=lambda a: self._features_costs[a])
         while len(available_features) and maximal_cost:
