3bb8bb20ed64fcce6608fc9d34e3c26a496b42d5
sivanyo <57237255+sivanyo@users.noreply.github.com>
2021-08-19 18:00:12 +0300

    change classes hierarchy

--- mid_algorithm.py
+++ mid_algorithm.py
@@ -23,7 +23,6 @@ class MaxVarianceAlgorithm(SequenceAlgorithm):
         self._train_samples = None
         self._features_costs = None
         self._features_by_corr = None
-        self._learning_algorithm = learning_algorithm
 
     def fit(self, train_samples: TrainSamples, features_costs: list[float]):
         """
@@ -38,19 +37,6 @@ class MaxVarianceAlgorithm(SequenceAlgorithm):
         correlations = np.var(train_samples.samples, axis=0)
         args_sort = np.argsort(correlations)
         self._features_by_corr = np.array(args_sort[::-1]).tolist()
-
-    def predict(self, samples: TestSamples, given_features: list[int], maximal_cost: float) -> Classes:
-        """
-        Predicts the class labels for the provided data. the function runs the fit and predict function of the given
-        sklearn's classifier on the given test samples filtering the given features only.
-        :param samples: test samples of shape (n_samples, n_features), i.e samples are in the rows.
-        :param given_features: list of the indices of the chosen features.
-        :param maximal_cost: the maximum available cost for buying features.
-        :return: Classes of shape (n_samples,) contains the class labels for each data sample.
-        """
-        given_features = self._buy_features(given_features, maximal_cost)
-        self._learning_algorithm.fit(self._train_samples.samples[:, given_features], self._train_samples.classes)
-        return self._learning_algorithm.predict(samples[:, given_features])
 
     # Private Methods
     def _buy_features(self, given_features: list[int], maximal_cost: float) -> list[int]:
